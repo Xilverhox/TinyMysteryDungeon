ea2f95a5d8b473fe3c5db01fbdcf3be3db158d2d
Xilverhox <xilverhox@gmail.com>
2016-09-19 11:40:34 +0900

    2016/9/19

---
+++ js/plugins/TinyMysteryDungeon.js
@@ -0,0 +1,22 @@
+﻿/*:
+ * @plugindesc 小さな不思議の迷宮専用プラグイン
+ * @author Silver.Fox　present kotonoha
+ * @help このゲーム専用に作成された初期読み込み用プラグイン
+ */
+
+(function() {
+// エンカウント率調整
+    Game_Player.prototype.makeEncounterCount = function() {
+    var n = $gameMap.encounterStep();
+    this._encounterCount = Math.randomInt(5) + n;
+    };
+// クリティカルのダメージ率調整
+    Game_Action.prototype.applyCritical = function(damage) {
+        return damage * 2;
+    };
+//YEP_BattleEnginecoreのウェイト調整
+Window_BattleLog.prototype.messageSpeed = function() {
+    return 8;
+};
+
+})();
